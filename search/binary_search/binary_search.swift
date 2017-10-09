447fb23d7cac5336b782679489d62747617358a2
Juris K <31712822+JurisKi@users.noreply.github.com>
2017-10-09 10:58:28 +0300

    Update binary_search.swift

--- search/binary_search/binary_search.swift
+++ search/binary_search/binary_search.swift
@@ -1,39 +1,8 @@
-/**
- Binary Search
- 
- Recursively splits the array in half until the value is found.
- 
- If there is more than one occurrence of the search key in the array, then
- there is no guarantee which one it finds.
- 
- Note: The array must be sorted!
- **/
+//binary search
 
 import Foundation
 
-// The recursive version of binary search.
-public func binarySearch<T: Comparable>(_ a: [T], key: T, range: Range<Int>) -> Int? {
-    if range.lowerBound >= range.upperBound {
-        return nil
-    } else {
-        let midIndex = range.lowerBound + (range.upperBound - range.lowerBound) / 2
-        if a[midIndex] > key {
-            return binarySearch(a, key: key, range: range.lowerBound ..< midIndex)
-        } else if a[midIndex] < key {
-            return binarySearch(a, key: key, range: midIndex + 1 ..< range.upperBound)
-        } else {
-            return midIndex
-        }
-    }
-}
-
-/**
- The iterative version of binary search.
- 
- Notice how similar these functions are. The difference is that this one
- uses a while loop, while the other calls itself recursively.
- **/
-
+// The iterative version of binary search
 public func binarySearch<T: Comparable>(_ a: [T], key: T) -> Int? {
     var lowerBound = 0
     var upperBound = a.count
@@ -49,3 +18,39 @@ public func binarySearch<T: Comparable>(_ a: [T], key: T) -> Int? {
     }
     return nil
 }
+
+//test
+let numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67]
+
+binarySearch(numbers, key: 43)  // gives 13
+
+// the recvursive version of binary search
+func binarySearch<T: Comparable>(_ a: [T], key: T, range: Range<Int>) -> Int? {
+    if range.lowerBound >= range.upperBound {
+        // If we get here, then the search key is not present in the array.
+        return nil
+
+    } else {
+        // Calculate where to split the array.
+        let midIndex = range.lowerBound + (range.upperBound - range.lowerBound) / 2
+
+        // Is the search key in the left half?
+        if a[midIndex] > key {
+            return binarySearch(a, key: key, range: range.lowerBound ..< midIndex)
+
+        // Is the search key in the right half?
+        } else if a[midIndex] < key {
+            return binarySearch(a, key: key, range: midIndex + 1 ..< range.upperBound)
+
+        // If we get here, then we've found the search key!
+        } else {
+            return midIndex
+        }
+    }
+}
+
+//test
+let numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67]
+
+binarySearch(numbers, key: 43)  // gives 13
+
