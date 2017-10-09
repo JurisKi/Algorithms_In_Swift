4e8e44748329c29ba515ed816b29a85cc9f91c87
Juris K <31712822+JurisKi@users.noreply.github.com>
2017-10-09 11:04:10 +0300

    Update binary_search.swift

--- search/binary_search/binary_search.swift
+++ search/binary_search/binary_search.swift
@@ -2,6 +2,7 @@
 
 import Foundation
 
+/*
 // The iterative version of binary search
 public func binarySearch<T: Comparable>(_ a: [T], key: T) -> Int? {
     var lowerBound = 0
@@ -20,29 +21,30 @@ public func binarySearch<T: Comparable>(_ a: [T], key: T) -> Int? {
 }
 
 //test
-let numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67]
+var numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67]
 
 binarySearch(numbers, key: 43)  // gives 13
+*/
 
 // the recvursive version of binary search
 func binarySearch<T: Comparable>(_ a: [T], key: T, range: Range<Int>) -> Int? {
     if range.lowerBound >= range.upperBound {
         // If we get here, then the search key is not present in the array.
         return nil
-
+        
     } else {
         // Calculate where to split the array.
         let midIndex = range.lowerBound + (range.upperBound - range.lowerBound) / 2
-
+        
         // Is the search key in the left half?
         if a[midIndex] > key {
             return binarySearch(a, key: key, range: range.lowerBound ..< midIndex)
-
-        // Is the search key in the right half?
+            
+            // Is the search key in the right half?
         } else if a[midIndex] < key {
             return binarySearch(a, key: key, range: midIndex + 1 ..< range.upperBound)
-
-        // If we get here, then we've found the search key!
+            
+            // If we get here, then we've found the search key!
         } else {
             return midIndex
         }
@@ -50,7 +52,7 @@ func binarySearch<T: Comparable>(_ a: [T], key: T, range: Range<Int>) -> Int? {
 }
 
 //test
-let numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67]
+var numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 33, 34, 37, 41, 43, 47, 53, 59, 61, 67]
 
-binarySearch(numbers, key: 43)  // gives 13
+binarySearch(numbers, key: 43, range: 0 ..< numbers.count)  // gives 15
 
